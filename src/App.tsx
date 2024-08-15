615e700edb15a7b4ed1534b78aadf99cca0c785c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-15 10:08:55 -0300

    put the app together with all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useState } from 'react'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { text: 'Do stuff', isCompleted: false },
+    { text: 'Make stuff', isCompleted: false },
+    { text: 'Gumber the lomb', isCompleted: false },
+  ])
+
+  const completeTodo = (index: number) => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (index: number) => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  const addTodo = (text: string) => {
+    const newTodos = [...todos, { text, isCompleted: false }]
+    setTodos(newTodos)
+  }
+
   return (
-    <div className='App'>
-      <h1>Todo List App</h1>
+    <div className='app'>
+      <h1>Todo List</h1>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   )
 }
